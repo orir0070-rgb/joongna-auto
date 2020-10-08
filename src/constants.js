8fb7deab6f336004086c95ee204cb33652f37410
Jeongtae Kim <rsatang5@naver.com>
2020-10-08 09:39:42 +0900

    constants.js 생성

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const writeUrl =
+  "https://m.cafe.naver.com/ca-fe/web/cafes/10050146/articles/write";
+export const loginUrl = "https://nid.naver.com/nidlogin.login";
+export const logoutUrl = "https://nid.naver.com/nidlogin.logout";
+export const userAgent = [
+  "Mozilla/5.0",
+  "(iPhone; CPU iPhone OS 14_0_1 like Mac OS X)",
+  "AppleWebKit/605.1.15",
+  "(KHTML, like Gecko)",
+  "Version/14.0",
+  "Mobile/15E148",
+  "Safari/604.1",
+].join(" ");
