ffa187fffd5771c53a80b8480ab42742fe0beb7c
Jeongtae Kim <rsatang5@naver.com>
2020-10-08 09:39:05 +0900

    WebView 컴포넌트 생성

---
+++ src/WebView.svelte
@@ -0,0 +1,21 @@
+<script>
+  let webview;
+
+  export let userAgent;
+
+  export function openDevTools() {
+    webview.openDevTools();
+  }
+  export function navigate(url) {
+    webview.src = url;
+  }
+</script>
+
+<webview 
+  bind:this={webview}
+  class={$$props.class}
+  useragent={userAgent}
+  partition="persist:webview"
+  nodeintegration={false}
+  enableremotemodule={false}
+/>
